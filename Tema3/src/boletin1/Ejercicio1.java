a24106a2c82d785309179f0577b489108cee2341
AlbaDuqueC <alba.duque@iesnervion.es>
2024-11-06 09:18:01 +0100

    ej 1

--- Tema3/src/boletin1/Ejercicio1.java
+++ Tema3/src/boletin1/Ejercicio1.java
@@ -1,9 +1,23 @@
 package boletin1;
 
+
+import java.util.Arrays;
+import java.util.Random;
+
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Variables
+		int tabla[] = new int[10];
+		
+		//creamos la utilidad random para general numero aleatorios
+		Random rand = new Random();
+		
+		for( int i=0 ; i<10; i++) {
+			tabla[i]= rand.nextInt(0, 101);
+			
+		}
+		System.out.println(Arrays.toString(tabla));
 
 	}
 
