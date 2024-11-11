ed3e4e6c48239ffaa5971272a6e4e55a7b4487fc
AlbaDuqueC <alba.duque@iesnervion.es>
2024-11-11 11:05:32 +0100

    exercise

---
+++ Tema3/src/english/Exercise1.java
@@ -0,0 +1,45 @@
+package english;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		// Variables
+		
+		int numbers[]= new int [10];
+		String answer;
+		int position;
+		int value;
+		
+		Scanner sc= new Scanner(System.in);
+		
+		do {
+			
+		System.out.println("A_Show values.");
+		System.out.println("B_Introduce a value.");
+		System.out.println("C_Exit.");
+		
+		answer= sc.nextLine();
+		
+		if(answer.equalsIgnoreCase("a")){
+			System.out.println(Arrays.toString(numbers));
+			
+		}else if(answer.equalsIgnoreCase("b")) {
+			
+			System.out.println("");
+			value= sc.nextInt();
+			System.out.println("");
+			position= sc.nextInt();
+			
+			System.out.println();
+		}
+		
+		
+		
+		}while(answer.equalsIgnoreCase("a") || answer.equalsIgnoreCase("b"));
+
+	}
+
+}
