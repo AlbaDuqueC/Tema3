50fa45456ef8087698be74001a71f235d44ba01f
AlbaDuqueC <alba.duque@iesnervion.es>
2024-11-12 14:09:26 +0100

    6

--- Tema3/src/boletin2/Ejercicio6.java
+++ Tema3/src/boletin2/Ejercicio6.java
@@ -1,13 +1,45 @@
 package boletin2;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Ejercicio6 {
 
 	public static void main(String[] args) {
 		// Variables
 
-		int tabla1[]= new int[6];
+		int tabla1[] = new int[6];
 
-		int tabla2[]= new int [6];
+		int tabla2[] = {3, 8, 12, 34, 43, 48};
+		int busqueda;
+		int i=0;
+		int num;
+		int numAciertos=0;
+		// Crea un numero random el cual se añadira a la tabla
+		Random rand = new Random();
+		
+		while ( i < 6 ) {
+
+			num = rand.nextInt(1, 50);
+			
+
+			tabla1[i] = num;
+			Arrays.sort(tabla1);
+			busqueda= Arrays.binarySearch(tabla1, num);
+			if(busqueda<0) {
+				tabla1[0]=num;
+				i++;
+			}
+			
+			
+		}
+		System.out.println(Arrays.toString(tabla1));
+		
+		for(int valor: tabla1) {
+			busqueda= Arrays.binarySearch(tabla2, valor);
+			numAciertos++;
+		}
+		System.out.println("Numero de aciertos: " + numAciertos);
 		
 		
 
