ec7307fce22d200fd344272523437b65b1c0773d
AlbaDuqueC <alba.duque@iesnervion.es>
2024-11-15 14:39:49 +0100

    boletin 4

--- Tema3/src/boletin3/Ejercicio5.java
+++ Tema3/src/boletin3/Ejercicio5.java
@@ -7,14 +7,22 @@ public class Ejercicio5 {
 	public static void main(String[] args) {
 		// Variables
 
+		//Esta tabla almacenara los datos que posteriormente si introduciran
 		int tabla[][] = new int[4][5];
 
+		//Creamos un rand para que introduzca posteriormente numero randoms a la tabla
 		Random rand = new Random();
 
+		//Esta variable sirve para poner el total de las filas
 		int totalI = 0;
+		
+		//Esta variable sirve para almacenar el total de las columnas
 		int totalJ = 0;
+		
+		//Esta variable es la suma total de todos los numeros aleatorios que se introduciran
 		int total = 0;
 
+		// Este bucle servira para introducir todos los valores que se introduciran en  
 		for (int i = 0; i < tabla.length; i++) {
 			for (int j = 0; j < tabla[i].length; j++) {
 
---
+++ Tema3/src/boletin4/Ejercicio1.java
@@ -0,0 +1,26 @@
+package boletin4;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		// variables
+		
+		int tabla[]= {1,1,1,1,1,1};
+		
+		int suma= sumaTabla(tabla);
+		
+		System.out.println(suma);
+
+	}
+	
+	static int sumaTabla(int tabla[]) {
+		
+		int suma=0;
+		for(int valor : tabla) {
+			suma+=valor;
+		}
+		
+		return (suma);
+	}
+
+}
