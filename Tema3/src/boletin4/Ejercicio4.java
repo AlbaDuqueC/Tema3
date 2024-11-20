4eeb0f64a8ceac9786b4171c84a9cd064ea08aaf
AlbaDuqueC <alba.duque@iesnervion.es>
2024-11-20 09:04:14 +0100

    Ej 4 y 5

--- Tema3/src/boletin4/Ejercicio4.java
+++ Tema3/src/boletin4/Ejercicio4.java
@@ -1,10 +1,43 @@
 package boletin4;
 
+
+import java.util.Scanner;
+
 public class Ejercicio4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Variables
+		int t[] = { 1, 2, 3, 4, 5, 6, 7, 3, 2, 2, 2, 3 };
 
+		int clave;
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Que valor quieres buscar?");
+
+		clave = sc.nextInt();
+
+		int resultado = buscarTodos(t, clave);
+
+		System.out.println("Estan en la posición: " + (resultado));
+
+		sc.close();
+
+	}
+
+	static int buscarTodos(int t[], int clave) {
+
+		int buscar = 0;
+
+		while (buscar < t.length && clave != t[buscar]) {
+			buscar++;
+		}
+
+		if (buscar >= t.length) {
+			buscar = -1;
+		}
+
+		return buscar;
 	}
 
 }
--- Tema3/src/boletin4/Ejercicio5.java
+++ Tema3/src/boletin4/Ejercicio5.java
@@ -7,70 +7,51 @@ public class Ejercicio5 {
 
 	public static void main(String[] args) {
 		// Variables
-		int t[]= {1,2,3,4,5,6,7,3,2,2,2,3};
-		
+		int t[] = { 1, 2, 3, 4, 5, 6, 7, 3, 2, 2, 2, 3 };
+
 		int valor;
-			
-		Scanner sc= new Scanner(System.in);
-		
+
+		Scanner sc = new Scanner(System.in);
+
 		System.out.println("Que valor quieres buscar?");
-		
-		valor= sc.nextInt();
-		
-		
-		
-		int resultado[]= buscarTodos(t, valor);
-		
-		
-		
-		System.out.println("Estan en la posición: "+ Arrays.toString(resultado));
-		
+
+		valor = sc.nextInt();
+
+		int resultado[] = buscarTodos(t, valor);
+
+		System.out.println("Estan en la posición: " + Arrays.toString(resultado));
+
 		sc.close();
 
 	}
-	
-	
-	
-	static int [] buscarTodos(int t[], int valor) {
-		int tamaño= numVeces(t, valor);
-		int [] tabla= new int [tamaño];
-		
-		for(int i=0; i<t.length; i++) {
-			
-			if (t[i]== valor) {
-				
-				for(int j=0; j<tabla.length; j++) {
-					
-					tabla[j]=i;
-					
-				
-				}
-			}
-		}
-		
-		return tabla;
-	}
-	
-	
-	static int numVeces(int t[], int valor) {
-		int cont =0;
-		for (int contenido : t) {
-			if (contenido==valor) {
+
+	static int[] buscarTodos(int t[], int valor) {
+		int tamaño = numVeces(t, valor);
+		int[] tabla = new int[tamaño];
+		int cont = 0;
+
+		for (int i = 0; i < t.length; i++) {
+
+			if (t[i] == valor) {
+
+				tabla[cont] = i;
+
 				cont++;
 			}
 		}
-		
-		return cont;
+
+		return tabla;
 	}
-	
-	
-	
-	static int[] posicion(int t[], int valor, int cuantos) {
-		int lugar[] = new int [cuantos];
-		
-		
-		
-		return lugar;
+
+	static int numVeces(int t[], int valor) {
+		int cont = 0;
+		for (int contenido : t) {
+			if (contenido == valor) {
+				cont++;
+			}
+		}
+
+		return cont;
 	}
 
 }
