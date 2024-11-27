e0a4886c923b2b6f7fb552351f3da1b5f550cff0
AlbaDuqueC <alba.duque@iesnervion.es>
2024-11-27 09:50:08 +0100

    Ej 1

---
+++ Tema3/src/boletin5/Ejercicio1.java
@@ -0,0 +1,82 @@
+package boletin5;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Ejercicio1 {
+
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		// Variables
+		int tabla[][] = dimensiones();
+
+		int num = valor();
+
+		boolean estar = false;
+
+		for (int i = 0; i < tabla.length; i++) {
+
+			for (int j = 0; j < tabla[i].length; j++) {
+
+				System.out.print(tabla[i][j] + "\t");
+				
+				if (tabla[i][j]== num) {
+					
+					estar=true;
+				}
+			}
+			System.out.println();
+		}
+
+		System.out.println();
+
+		if (estar) {
+
+			System.out.println("Esta en la tabla");
+
+		} else if (!estar) {
+
+			System.out.println("No esta en la tabla");
+
+		}
+
+	}
+
+	static int[][] dimensiones() {
+		int tabla1[][];
+		int colum;
+		int fila;
+
+		Random rand = new Random();
+
+		System.out.println("introduce el numero de columnas");
+		colum = sc.nextInt();
+
+		System.out.println("Introduce las filas");
+		fila = sc.nextInt();
+
+		tabla1 = new int[fila][colum];
+
+		for (int i = 0; i < fila; i++) {
+
+			for (int j = 0; j < colum; j++) {
+
+				tabla1[i][j] = rand.nextInt(0, 10);
+			}
+		}
+
+		return tabla1;
+	}
+
+	static int valor() {
+		int valor;
+
+		System.out.println("Introduce el valor que quiera buscar en la tabla:");
+		valor = sc.nextInt();
+
+		return valor;
+
+	}
+
+}
