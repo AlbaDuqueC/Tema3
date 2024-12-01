d4597f3e71d2c821be7cd54d7f90513ab94089df
AlbaDuqueC <alba.duque@iesnervion.es>
2024-12-01 13:50:43 +0100

    4 casi

---
+++ Tema3/src/boletin5/Ejercicio4.java
@@ -0,0 +1,341 @@
+package boletin5;
+
+import java.util.Scanner;
+
+public class Ejercicio4 {
+
+	public static void main(String[] args) {
+		// Variables
+
+		String[][] tablero = new String[8][8];
+
+		int posFila;
+
+		int posColumna;
+
+		char pieza;
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("¿Que pieza es?");
+
+		pieza = sc.next().toUpperCase().charAt(0);
+
+		System.out.println("¿En que fila esta?");
+
+		posFila = sc.nextInt();
+
+		System.out.println("¿En que columna hay?");
+
+		posColumna = sc.nextInt();
+
+		switch (pieza) {
+
+		case 'T' -> {
+
+			tablero = Torre(tablero, posFila, posColumna);
+
+			for (int i = 0; i < tablero.length; i++) {
+				for (int j = 0; j < tablero[i].length; j++) {
+
+					System.out.print(tablero[i][j]);
+
+				}
+				System.out.println();
+				System.out.println();
+			}
+
+		}
+		case 'A' -> {
+
+			tablero = Alfil(tablero, posFila, posColumna);
+
+			for (int i = 0; i < tablero.length; i++) {
+				for (int j = 0; j < tablero[i].length; j++) {
+
+					System.out.print(tablero[i][j]);
+
+				}
+				System.out.println();
+				System.out.println();
+			}
+
+		}
+		case 'D' -> {
+
+			tablero = Dama(tablero, posFila, posColumna);
+
+			for (int i = 0; i < tablero.length; i++) {
+				for (int j = 0; j < tablero[i].length; j++) {
+
+					System.out.print(tablero[i][j]);
+
+				}
+				System.out.println();
+				System.out.println();
+			}
+
+		}
+		case 'C' -> {
+
+			tablero = Caballo(tablero, posFila, posColumna);
+
+			for (int i = 0; i < tablero.length; i++) {
+				for (int j = 0; j < tablero[i].length; j++) {
+
+					System.out.print(tablero[i][j]);
+
+				}
+				System.out.println();
+				System.out.println();
+			}
+
+		}
+		default -> {
+			System.out.println("NADA");
+		}
+
+		}
+
+		sc.close();
+
+	}
+
+	static String[][] Torre(String tablero[][], int posFila, int posColumna) {
+		String[][] tableroT = new String[8][8];
+
+		tableroT[posFila][posColumna] = "  T  ,";
+
+		for (int j = 0; j < tableroT.length; j++) {
+
+			if (j != posColumna) {
+
+				tableroT[posFila][j] = "  X  ,";
+			}
+		}
+
+		for (int i = 0; i < tableroT.length; i++) {
+			if (i != posFila) {
+				tableroT[i][posColumna] = "  X  ,";
+			}
+		}
+
+		for (int i = 0; i < tableroT.length; i++) {
+
+			for (int j = 0; j < tableroT[i].length; j++) {
+
+				if (tableroT[i][j] == null) {
+					tableroT[i][j] = "     ,";
+
+				}
+			}
+
+		}
+
+		return tableroT;
+	}
+
+	static String[][] Alfil(String tablero[][], int posFila, int posColumna) {
+
+		String[][] tableroA = new String[8][8];
+
+		tableroA[posFila][posColumna] = "  A  ,";
+
+		int cont = 0;
+
+		int colum = posColumna;
+
+		int fila = posFila;
+
+		for (int i = fila; i >= 0; i--) {
+
+			cont = 0;
+
+			for (int j = colum; j >= 0; j--) {
+
+				if (i != fila && j != posColumna) {
+
+					if (cont == 0) {
+
+						tableroA[i][j] = "  X  ,";
+
+						cont++;
+					}
+
+				}
+
+			}
+			colum--;
+		}
+
+		colum = posColumna;
+
+		for (int i = fila; i >= 0; i--) {
+
+			cont = 0;
+
+			for (int j = colum; j < tableroA[i].length; j++) {
+
+				if (i != fila && j != posColumna) {
+
+					if (cont == 0) {
+
+						tableroA[i][j] = "  X  ,";
+
+						cont++;
+					}
+
+				}
+
+			}
+			colum++;
+
+		}
+		colum = posColumna;
+
+		for (int i = fila; i < tableroA.length; i++) {
+
+			cont = 0;
+
+			for (int j = colum; j >= 0; j--) {
+				if (i != fila && j != posColumna) {
+
+					if (cont == 0) {
+
+						tableroA[i][j] = "  X  ,";
+
+						cont++;
+					}
+
+				}
+
+			}
+			colum--;
+
+		}
+
+		colum = posColumna;
+
+		for (int i = fila; i < tableroA.length; i++) {
+
+			cont = 0;
+
+			for (int j = colum; j < tableroA[i].length; j++) {
+
+				if (i != fila && j != posColumna) {
+
+					if (cont == 0) {
+
+						tableroA[i][j] = "  X  ,";
+
+						cont++;
+					}
+
+				}
+
+			}
+			colum++;
+
+		}
+
+		for (int i = 0; i < tableroA.length; i++) {
+
+			for (int j = 0; j < tableroA[i].length; j++) {
+
+				if (tableroA[i][j] == null) {
+
+					tableroA[i][j] = "     ,";
+
+				}
+			}
+
+		}
+
+		return tableroA;
+	}
+
+	static String[][] Dama(String tablero[][], int posFila, int posColumna) {
+
+		String[][] tableroD = new String[8][8];
+
+		tableroD = Alfil(tablero, posFila, posColumna);
+
+		tableroD[posFila][posColumna] = "  D  ,";
+
+		for (int j = 0; j < tableroD.length; j++) {
+
+			if (j != posColumna) {
+
+				tableroD[posFila][j] = "  X  ,";
+			}
+		}
+
+		for (int i = 0; i < tableroD.length; i++) {
+			if (i != posFila) {
+				tableroD[i][posColumna] = "  X  ,";
+			}
+		}
+
+		for (int i = 0; i < tableroD.length; i++) {
+
+			for (int j = 0; j < tableroD[i].length; j++) {
+
+				if (tableroD[i][j] == null) {
+					tableroD[i][j] = "     ,";
+
+				}
+			}
+
+		}
+
+		return tableroD;
+	}
+	
+	
+//Esta mal, si lo ònes en un sitio que no se pueden poner todas la X da error
+	static String[][] Caballo(String tablero[][], int posFila, int posColumna) {
+		String[][] tableroC = new String[8][8];
+
+		tableroC[posFila][posColumna] = "  C  ,";
+		
+		for (int i = 0; i < tableroC.length; i++) {
+
+			for (int j = 0; j < tableroC[i].length; j++) {
+
+
+		tableroC[posFila - 1][posColumna - 2] = "  X  ,";
+
+		tableroC[posFila - 2][posColumna - 1] = "  X  ,";
+
+		tableroC[posFila - 1][posColumna + 2] = "  X  ,";
+
+		tableroC[posFila - 2][posColumna + 1] = "  X  ,";
+
+		tableroC[posFila + 1][posColumna - 2] = "  X  ,";
+
+		tableroC[posFila + 2][posColumna - 1] = "  X  ,";
+
+		tableroC[posFila + 1][posColumna + 2] = "  X  ,";
+
+		tableroC[posFila + 2][posColumna + 1] = "  X  ,";
+		
+			}}
+			
+
+		for (int i = 0; i < tableroC.length; i++) {
+
+			for (int j = 0; j < tableroC[i].length; j++) {
+
+				if (tableroC[i][j] == null) {
+					tableroC[i][j] = "     ,";
+
+				}
+			}
+
+		}
+
+		return tableroC;
+
+	}
+
+}
