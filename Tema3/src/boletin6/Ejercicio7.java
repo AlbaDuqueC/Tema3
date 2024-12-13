58a07382443dc390931a6059f72d0faa05567cc0
AlbaDuqueC <alba.duque@iesnervion.es>
2024-12-13 14:37:42 +0100

    7

---
+++ Tema3/src/boletin6/Ejercicio7.java
@@ -0,0 +1,64 @@
+package boletin6;
+
+import java.util.Scanner;
+
+public class Ejercicio7 {
+
+	public static void main(String[] args) {
+		// Variables
+
+		String frase;
+
+		String palabra;
+		
+		int cont=0;
+		
+		int numpalabra=0;
+		
+		int k;
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Introduzca una frase: ");
+
+		frase = sc.nextLine();
+
+		System.out.println("Introducir una palabra: ");
+
+		palabra = sc.nextLine();
+
+		for (int i = 0; i < frase.length(); i++) {
+			
+			k=i;
+
+			if (frase.charAt(i) == palabra.charAt(0)) {
+
+				for (int j = 0; j < palabra.length(); j++) {
+					
+					
+
+					if(frase.charAt(i) == palabra.charAt(j)) {
+						cont++;
+						
+						i++;
+					}
+					if(cont==palabra.length()) {
+						
+						numpalabra++;
+						
+						
+					}
+					
+					
+				}				
+				
+			}
+			
+			i=k;
+
+		}
+
+		System.out.println(numpalabra);
+	}
+
+}
