2dc451b3c2cc0cf2f8c45ebcfb10a4e3bb190ab1
AlbaDuqueC <alba.duque@iesnervion.es>
2025-01-12 14:34:16 +0100

    mas y mas

--- Tema3/src/boletin6/Ejercicio17.java
+++ Tema3/src/boletin6/Ejercicio17.java
@@ -1,12 +1,42 @@
 package boletin6;
 
+import java.util.Scanner;
+
 public class Ejercicio17 {
 
 	public static void main(String[] args) {
-		// Variables 
-		
-		
+		// Variables
+		// código con comentarios
+		String codigo;
 
+		//Codigo sin
+		String codigoSinComentarios;
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Introduzca el codigo C:");
+
+		codigo = sc.nextLine();
+
+		// eliminar los comentarios
+		codigoSinComentarios = eliminarComentarios(codigo);
+
+		// Imprimimos el código sin comentarios
+		System.out.println("Código sin comentarios:");
+
+		System.out.println(codigoSinComentarios);
+	}
+
+	// Función que elimina los comentarios
+	public static String eliminarComentarios(String codigo) {
+		
+		// Elimina los comentarios de bloque /* ... */
+		codigo = codigo.replaceAll("/\\*.*?\\*/", "");
+
+		// Elimina los comentarios de una sola línea // ...
+		codigo = codigo.replaceAll("//.*", "");
+
+		return codigo;
 	}
 
 }
